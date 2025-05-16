e0a1d631b9ef21110cf56b77016a5caf31c35570
Vinicius Valoci Gaioli <Vinimancabbr@gmail.com>
2025-05-16 01:00:54 -0300

    Discrete Wavelet Transform first implementation and some tests

--- algorithms.py
+++ algorithms.py
@@ -2,13 +2,13 @@ import cv2 as cv
 import numpy as np
 import os
 import tempfile
+import matplotlib.pyplot as plt
+import pywt
 
 image = cv.imread("imageTest\\real-nature-hd-1920x1200-wallpaper-preview.jpg")
-
-
 #Debug functions  ------------------------------
 def getYCbCrSizeData(path, Y, Cb, Cr):
-    np.savez_compressed(path, Y=Y, Cb=Cb, Cr=Cr)
+    np.savez(path, Y=Y, Cb=Cb, Cr=Cr)
     size_bytes = os.path.getsize(path + ".npz")
     size_kb = size_bytes / 1024
     size_mb = size_kb / 1024
@@ -46,6 +46,29 @@ def subSampleChrominance(channel, factor):
 
     return result
 
+#Discrete Wavelet Transform Compression (code adapted from: https://www.youtube.com/watch?v=eJLF9HeZA8I&ab_channel=SteveBrunton)
+def DWTCompression(channel, factor=2, wavelet='haar', threshHold=0.05):
+    '''
+        Haar wavelet is one of the main choices for image compression
+    '''
+    coeffs = pywt.wavedec2(channel, wavelet=wavelet, level=factor)
+    coeffArr, coeffSlices, = pywt.coeffs_to_array(coeffs)
+    #TreshHolding ---------------------
+    CoeffSort = np.sort(np.abs(coeffArr.reshape(-1)))
+    thresh = CoeffSort[int(np.floor((threshHold) * len(CoeffSort)))]
+    ind = np.abs(coeffArr) > thresh
+    
+    Cfiltered = coeffArr * ind #treshold small indices
+    
+    return Cfiltered, coeffSlices, wavelet
+    
+def DWTDecompression(Cfiltered, coeffSlices, wavelet='haar'): 
+    coeffsThresholded = pywt.array_to_coeffs(Cfiltered, coeffSlices, output_format='wavedec2')
+    reconstructedImage = pywt.waverec2(coeffsThresholded, wavelet=wavelet)
+    
+    return reconstructedImage.astype('uint8')
+    
+    
 """
     Usually, the chrominance downsampling factor is fixed at 2, which essentially halves the file size. Since we have three channels (Y, Cb, Cr) 
     that we can consider to be of size "X", when we divide the rows and columns of the chrominance channels by 2, we get: X + X/4 + X/4 = 1.5X, 
@@ -54,42 +77,71 @@ def subSampleChrominance(channel, factor):
     
 """
 Y, Cb, Cr = YCbCrImage(image)
-getYCbCrSizeData("original_444", Y, Cb, Cr)
+#getYCbCrSizeData("original_444", Y, Cb, Cr)
+
+cv.imshow("Original image", image)
+
+#Discrete Wavelet Transfornm test ------------------------------
+
+#cv.imshow("DWT not compressed", Y)
+
+dwtImage, coeffSlices, wavelet = DWTCompression(Y, 4, 'db1', 0.1)
+reconstructedImage = DWTDecompression(dwtImage, coeffSlices, wavelet)
+
+Y2, Cb2, Cr2 = YCbCrImage(reconstructedImage)
+
+YResized = cv.resize(Y2, (image.shape[1], image.shape[0]), interpolation=cv.INTER_CUBIC)
+
+reconstructedImage = cv.merge([YResized, Cb, Cr])
+reconstructedImage = cv.cvtColor(reconstructedImage, cv.COLOR_YCrCb2BGR)
+cv.imshow("Y DWT compressed", reconstructedImage)
+#cv.imshow("DWT compressed image", reconstructedImage)
 
 
-#Downsampling test
-'''Cb = downSampleChrominance(Cb, 2)
-Cr = downSampleChrominance(Cr, 2)
+#Downsampling test ------------------------------
+'''
+CbDownSample = downSampleChrominance(Cb, 2)
+CrDownSample = downSampleChrominance(Cr, 2)
 '''
 
 
 
-#Subsampling teste
+
+#Subsampling test------------------------------
 '''
-CbSubSampled = subSampleChrominance(Cb, 2)
-CrSubSampled = subSampleChrominance(Cr, 2)
+CbSubSampled = subSampleChrominance(Cb, 100)
+CrSubSampled = subSampleChrominance(Cr, 100)
 '''
 
-getYCbCrSizeData("subsampled_420", Y, CbSubSampled, Cr)
+#getYCbCrSizeData("subsampled_420", Y, CbDownSample, CrDownSample)
 
 
 #Rebuilding image for visualization ------------------------------
-CbResized = cv.resize(Cb, (image.shape[1], image.shape[0]), interpolation=cv.INTER_CUBIC)
-CrResized = cv.resize(Cr, (image.shape[1], image.shape[0]), interpolation=cv.INTER_CUBIC)
+'''
+CbResized = cv.resize(CbDownSample, (image.shape[1], image.shape[0]), interpolation=cv.INTER_CUBIC)
+CrResized = cv.resize(CrDownSample, (image.shape[1], image.shape[0]), interpolation=cv.INTER_CUBIC)
+'''
+
+
+#Subsampling visualization ------------------------------
+'''
+cv.imshow("Chroma blue original: ", Cb)
+cv.imshow("Chroma blue subsample", CbResized)
+cv.imshow("Chroma red original: ", Cr)
+cv.imshow("Chroma red subsample", CrResized)
+cv.imshow("Y: ", Y)
+'''
 
 
 
-#Subsampling visualization
-#cv.imshow("Chroma blue original: ", Cb)
-#cv.imshow("Chroma blue subsample", CbResized)
-#cv.imshow("Chroma red original: ", Cr)
-#cv.imshow("Chroma red subsample", CrResized)
+#Image visualization:
 
+'''
 reconstructedImage = cv.merge([Y, CbResized, CrResized])
 reconstructedImage = cv.cvtColor(reconstructedImage, cv.COLOR_YCrCb2BGR)
 
-#Image visualization:
-#cv.imshow("Original image", image)
-#cv.imshow("Reconstructed image", reconstructedImage)
+cv.imshow("Original image", image)
+cv.imshow("Reconstructed image", reconstructedImage)
+'''
 
 cv.waitKey(0)
