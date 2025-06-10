ce2dfba3c8cc4bf27973a43f9cf2587caf2df3fa
Vinicius Valoci Gaioli <Vinimancabbr@gmail.com>
2025-06-10 16:06:41 -0300

    Mudança de coluna da esquerda

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from algorithms import *
 
 global imagem
 imagem = None
+
+
+
+
+        
 def inserirImagem():
     caminho = filedialog.askopenfilename(
         filetypes=[("Imagens", "*.png *.jpg *.jpeg *.gif *.bmp")]
@@ -25,6 +30,54 @@ def redimensionar(imagem, larguraMax, alturaMax):
     novaLargura = int(larguraOriginal * proporcao)
     novaAltura = int(alturaOriginal * proporcao)
     return imagem.resize((novaLargura, novaAltura), Image.Resampling.LANCZOS)
+
+def DWTLeftColumn():
+    global slider1
+    global slider2
+    global subImagemLabel
+    slider1 = None
+    slider2 = None
+    subImagemLabel = None
+    titleLabel = tk.Label(frameDiv, text="DWT (Transformada Wavelet Discreta)", justify='left', font=("TkDefaultFont", 10))
+    titleLabel.grid(row=1, column=0, columnspan=2, sticky='w', pady=10)
+
+    textLabel = tk.Label(frameDiv, text="A Compressão de Imagens com DWT é uma técnica moderna e eficiente para"  
+    "reduzir o tamanho de imagens digitais, preservando a qualidade visual.  Quando a DWT  é aplicada em um canal" 
+    "de uma imagem, representamos a imagem original em um formato de multiresolução, onde separamos a base estrutural"  
+    "de uma imagem de seus detalhes finos e ruídos."
+    "\n\n\nFator é o valor responsável por determinar o número de iterações da decomposição DWT: ", wraplength=300, justify='left')
+    textLabel.grid(row=2, column=0, columnspan=2, sticky='we')
+
+    textLabelFactor = tk.Label(frameDiv, text="Factor: ")
+    textLabelFactor.grid(row=3, column=0, sticky='ws')
+    slider1 = tk.Scale(frameDiv,
+                    from_=1, to=100,     
+                    orient="horizontal",  
+                    command=sliderChange)     
+    slider1.grid(row=3, column=1, sticky='we')
+
+    subImagemLabel = tk.Label(frameDiv)
+    subImagemLabel.grid(row=4, column=0, columnspan=2, sticky='w')
+
+    textLabel2 = tk.Label(frameDiv, text="\n\nO treshhold determina qual a porcentagem de altas frequências serão descartadas: ", wraplength=300, justify='left')
+    textLabel2.grid(row=5, column=0, columnspan=2, sticky='w')
+
+
+    textLabelTreshhold = tk.Label(frameDiv, text="TreshHold: ")
+    textLabelTreshhold.grid(row=6, column=0, sticky='ws')
+
+    slider2 = tk.Scale(frameDiv,
+                    from_= 5, to=999,     
+                    orient="horizontal",  
+                    command=sliderChange)     
+    slider2.grid(row=6, column=1, sticky='we')
+    
+
+def showLeftColumn(event=None):
+    option = dropdownBtn.get()
+    if option == "DWT (Transformada Wavelet Discreta)":
+        DWTLeftColumn()
+
 
 def sliderChange(valor):
     global imagem
@@ -119,7 +172,7 @@ frameDireita = tk.Frame(root, bg='green')
 frameDireita.grid_columnconfigure(0, weight=1)
 frameDireita.grid(row=0, column=1, sticky='nswe')
 
-iconeInserir = Image.open("assets\\openFolder.png")
+iconeInserir = Image.open("waveletFractalCompression\\assets\\openFolder.png")
 iconeInserir = iconeInserir.resize((24, 24))
 iconeInserir = ImageTk.PhotoImage(iconeInserir)
 
@@ -136,41 +189,7 @@ values = ["DWT (Transformada Wavelet Discreta)", "Subsample dos canais chroma", 
 dropdownBtn = ttk.Combobox(frameDiv, values=values)
 dropdownBtn.current(0)
 dropdownBtn.grid(row=0, column=1, sticky='w')
-
-titleLabel = tk.Label(frameDiv, text="DWT (Transformada Wavelet Discreta)", justify='left', font=("TkDefaultFont", 10))
-titleLabel.grid(row=1, column=0, columnspan=2, sticky='w', pady=10)
-
-textLabel = tk.Label(frameDiv, text="A Compressão de Imagens com DWT é uma técnica moderna e eficiente para"  
-"reduzir o tamanho de imagens digitais, preservando a qualidade visual.  Quando a DWT  é aplicada em um canal" 
-"de uma imagem, representamos a imagem original em um formato de multiresolução, onde separamos a base estrutural"  
-"de uma imagem de seus detalhes finos e ruídos."
-"\n\n\nFator é o valor responsável por determinar o número de iterações da decomposição DWT: ", wraplength=300, justify='left')
-textLabel.grid(row=2, column=0, columnspan=2, sticky='we')
-
-textLabelFactor = tk.Label(frameDiv, text="Factor: ")
-textLabelFactor.grid(row=3, column=0, sticky='ws')
-
-slider1 = tk.Scale(frameDiv,
-                from_=1, to=100,     
-                orient="horizontal",  
-                command=sliderChange)     
-slider1.grid(row=3, column=1, sticky='we')
-
-subImagemLabel = tk.Label(frameDiv)
-subImagemLabel.grid(row=4, column=0, columnspan=2, sticky='w')
-
-textLabel2 = tk.Label(frameDiv, text="\n\nO treshhold determina qual a porcentagem de altas frequências serão descartadas: ", wraplength=300, justify='left')
-textLabel2.grid(row=5, column=0, columnspan=2, sticky='w')
-
-
-textLabelTreshhold = tk.Label(frameDiv, text="TreshHold: ")
-textLabelTreshhold.grid(row=6, column=0, sticky='ws')
-
-slider2 = tk.Scale(frameDiv,
-                from_= 5, to=999,     
-                orient="horizontal",  
-                command=sliderChange)     
-slider2.grid(row=6, column=1, sticky='we')
+dropdownBtn.bind("<<ComboboxSelected>>", showLeftColumn)
 
 root.mainloop()
 
